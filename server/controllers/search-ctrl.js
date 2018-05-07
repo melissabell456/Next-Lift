7c72b2899629808c325f69f1aa29b024f84b83ee
Melissa Bell <melissabell456@gmail.com>
2018-05-07 15:12:41 -0500

    refactored pug template for forms to be dynamically created

--- server/controllers/search-ctrl.js
+++ server/controllers/search-ctrl.js
@@ -8,16 +8,17 @@ var sequelize = new Sequelize({
   "dialect": "postgres"
   }
 );
+const formAttributes = [{column: "type", value:"isolation"}, {column:"type", value:"compound"}, {column: "motion", value:"pull"}, {column:"motion", value:"push"}, {column:"region", value:"upper"}, {column:"region", value:"lower"}];
 
 module.exports.renderSearchView = (req, res, next) => {
-  res.render('search');
+  res.render('search', { formAttributes });
 }
 
 module.exports.searchLiftsTable = (req, res, next) => {
 
   sequelize.query(
     `SELECT lifts.name as title FROM lifts WHERE lifts.${req.query.column} ILIKE '%${req.query.term}%'`).spread((results, metadata) => {
-    res.render('search', { term: req.query.term, results, state: "query" });
+    res.render('search', { formAttributes, term: req.query.term, results, state: "query" });
   })
 
 }
