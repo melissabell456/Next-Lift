ad3d1d04e3f9fa9d37a744efc18bf51b897bc5ca
Melissa Bell <melissabell456@gmail.com>
2018-05-07 14:48:46 -0500

    search and filtering functionality returning appropriate data

--- server/controllers/search-ctrl.js
+++ server/controllers/search-ctrl.js
@@ -17,7 +17,6 @@ module.exports.searchLiftsTable = (req, res, next) => {
 
   sequelize.query(
     `SELECT lifts.name as title FROM lifts WHERE lifts.${req.query.column} ILIKE '%${req.query.term}%'`).spread((results, metadata) => {
-    console.log(results);
     res.render('search', { term: req.query.term, results, state: "query" });
   })
 
