65a9a8646ca6b0eba93fc2f4fff499c5dcddc9e1
Melissa Bell <melissabell456@gmail.com>
2018-05-09 14:43:07 -0500

    registration set up - user can register

--- server/routes/auth-route.js
+++ server/routes/auth-route.js
@@ -2,8 +2,10 @@
 
 const { Router } = require('express');
 const authRouter = Router();
-const { login, register } = require('../controllers/authCtrl');
+const { login, register, displayRegister } = require('../controllers/authCtrl');
 
 authRouter.post('/login', login);
+authRouter.get('/register', displayRegister);
+authRouter.post('/register', register);
 
 module.exports = authRouter;
