c0c7750b4c0f7803f729f3375b054a0aa209964d
Melissa Bell <melissabell456@gmail.com>
2018-05-11 10:33:37 -0500

    pug form template finally printing value to dom

--- server/controllers/log-ctrl.js
+++ server/controllers/log-ctrl.js
@@ -1,8 +1,34 @@
 'use strict';
 
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize({
+  "username": "Melis",
+  "password": "postgres",
+  "database": "final-wrkout",
+  "dialect": "postgres"
+  }
+);
+
 module.exports.renderLiftForm = (req, res, next) => {
   console.log(req.query);
-  res.render('log-lift-form', req.query);
+  const liftId = +(req.query.lift_id);
+  const equipId = +(req.query.equip_id);
+  sequelize.query(
+    `SELECT * 
+    FROM lift_and_equipment_combos ap
+    LEFT JOIN user_log ul ON ul.ul_lift_id = ap.wkout_id AND ul.ul_equip_id = ap.equip_id
+    LEFT JOIN user_lift u ON ul.ul_user_id = u.user_id 
+      AND ul.ul_lift_id = u.lift_id 
+      AND ul.ul_equip_id = u.equipment_id 
+      AND ul.liftDate = to_char(u."createdAt", 'MM-DD-YYYY')
+    WHERE (ul.ul_user_id = ${req.user.id} OR ul.ul_user_id IS NULL)
+      AND ap.wkout_id = ${liftId}
+	    AND ap.equip_id = ${equipId}`
+  ).spread( (results, metadata) => {
+    console.log(results);
+    res.render('log-lift-form', { results } );
+  })
+  // res.json(liftStats);
 } 
 
 module.exports.recordLift = (req, res, next) => {
--- server/controllers/search-ctrl.js
+++ server/controllers/search-ctrl.js
@@ -18,7 +18,7 @@ module.exports.searchLiftsTable = (req, res, next) => {
   sequelize.query(
     `SELECT * 
     FROM lift_and_equipment_combos ap
-    LEFT JOIN user_log ul ON ul.ul_lift_id = ap.wkout_id AND ul.ul_equip_id = ap.equipment_id
+    LEFT JOIN user_log ul ON ul.ul_lift_id = ap.wkout_id AND ul.ul_equip_id = ap.equip_id
     LEFT JOIN user_lift u ON ul.ul_user_id = u.user_id 
       AND ul.ul_lift_id = u.lift_id 
       AND ul.ul_equip_id = u.equipment_id 
@@ -30,23 +30,5 @@ module.exports.searchLiftsTable = (req, res, next) => {
     res.render('search', { formAttributes, term: req.query.term, results, state: "query", log_privilege: "true" });
   })
 
-  // `SELECT * 
-  // FROM lift_and_equipment_combos ap
-  // LEFT JOIN user_log ul ON ul.ul_lift_id = ap.lift_id AND ul.ul_equip_id = ap.equipment_id
-  // LEFT JOIN user_lift u ON ul.ul_user_id = u.user_id 
-	// AND ul.ul_lift_id = u.lift_id 
-	// AND ul.ul_equip_id = u.equipment_id 
-	// AND ul.liftDate = to_char(u."createdAt", 'MM-DD-YYYY')
-  // WHERE (ul.user_id = ${req.user.id} OR ul.user_id IS NULL)
-  // AND ap.${req.query.column} ILIKE '%${req.query.term}%'`
-  // `SELECT * 
-  // FROM lift_and_equipment_combos ap
-  // LEFT JOIN user_log ul ON ul.ul_lift_id = ap.lift_id AND ul.ul_equip_id = ap.equipment_id
-  // LEFT JOIN user_lift u ON ul.ul_user_id = u.user_id 
-	// AND ul.ul_lift_id = u.lift_id 
-	// AND ul.ul_equip_id = u.equipment_id 
-	// AND ul.liftDate = to_char(u."createdAt", 'MM-DD-YYYY')
-  // WHERE (ul.user_id = ${req.user.id} OR ul.user_id IS NULL)
-  // AND ap.${req.query.column} ILIKE '%${req.query.term}%'`
 }
 
