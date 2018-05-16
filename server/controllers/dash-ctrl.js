cc254d37235982608f048bada88516b30d079063
Melissa Bell <melissabell456@gmail.com>
2018-05-16 11:38:04 -0500

    displaying user's logged lifts - need to adjust column names for use in partial

--- server/controllers/dash-ctrl.js
+++ server/controllers/dash-ctrl.js
@@ -1,10 +1,32 @@
 'use strict'
 
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize({
+  "username": "Melis",
+  "password": "postgres",
+  "database": "final-wrkout",
+  "dialect": "postgres"
+  }
+);
+
 module.exports.renderDashView = (req, res, next) => {
-  res.render('user-dash');
-} 
-// SELECT *
-// FROM lift_and_equipment_combos lc
-// LEFT JOIN user_lift ul ON lc.wkout_id = ul.lift_id
-//   AND lc.equip_id = ul.equipment_id
-// WHERE ul.user_id = 1
+  getUserHistory(req.user.id)
+  .then( userLogs => {
+    res.render('user-dash', { userLogs });
+  })
+}
+
+const getUserHistory = (user_id) => {
+  return new Promise( (resolve, reject) => {
+    sequelize.query(
+      `SELECT *
+      FROM lift_and_equipment_combos lc
+      LEFT JOIN user_lift ul ON lc.wkout_id = ul.lift_id
+        AND lc.equip_id = ul.equipment_id
+      WHERE ul.user_id = ${user_id}
+      ORDER BY liftname`
+    ).spread( (results, metadata) => {
+      resolve(results);
+    })
+  })
+}
