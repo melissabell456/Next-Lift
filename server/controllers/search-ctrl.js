424271b7aec9ec42b9cc8191606fd503f4e46540
Melissa Bell <melissabell456@gmail.com>
2018-05-17 09:23:24 -0500

    muscle groups displayed on search results

--- server/controllers/search-ctrl.js
+++ server/controllers/search-ctrl.js
@@ -26,14 +26,27 @@ module.exports.searchLiftsTable = (req, res, next) => {
   sequelize.query(
     `SELECT * 
     FROM lift_and_equipment_combos ap
-    LEFT JOIN user_log ul ON ul.ul_lift_id = ap.wkout_id AND ul.ul_equip_id = ap.equip_id
+    LEFT JOIN user_log ul ON ul.ul_lift_id = ap.wkout_id 
+    AND ul.ul_equip_id = ap.equip_id
     LEFT JOIN user_lift u ON ul.ul_user_id = u.user_id 
-      AND ul.ul_lift_id = u.lift_id 
-      AND ul.ul_equip_id = u.equipment_id 
-      AND ul.liftDate = to_char(u."createdAt", 'MM-DD-YYYY')
+        AND ul.ul_lift_id = u.lift_id 
+        AND ul.ul_equip_id = u.equipment_id 
+        AND ul.liftDate = to_char(u."createdAt", 'MM-DD-YYYY')
+    LEFT JOIN lift_primary_muscle lpm ON lpm.lift_m_id = ap.wkout_id
+    LEFT JOIN lift_muscle_associations lma ON lpm.lift_m_id = lma.lift_m_id
     WHERE (ul.ul_user_id = ${req.user.id} OR ul.ul_user_id IS NULL)
-      AND ap.${req.query.column} ILIKE '%${req.query.term}%'`
+    AND ap.${req.query.column} ILIKE '%${req.query.term}%'`
+    // `SELECT * 
+    // FROM lift_and_equipment_combos ap
+    // LEFT JOIN user_log ul ON ul.ul_lift_id = ap.wkout_id AND ul.ul_equip_id = ap.equip_id
+    // LEFT JOIN user_lift u ON ul.ul_user_id = u.user_id 
+    //   AND ul.ul_lift_id = u.lift_id 
+    //   AND ul.ul_equip_id = u.equipment_id 
+    //   AND ul.liftDate = to_char(u."createdAt", 'MM-DD-YYYY')
+    // WHERE (ul.ul_user_id = ${req.user.id} OR ul.ul_user_id IS NULL)
+    //   AND ap.${req.query.column} ILIKE '%${req.query.term}%'`
     ).spread((results, metadata) => {
+      console.log(results, "do they have muscle groups?");
     res.render('search', { formAttributes, term: req.query.term, results, state: "query", log_privilege: "true" });
   })
 
@@ -47,8 +60,7 @@ module.exports.searchLiftsTable = (req, res, next) => {
 //     AND ul.ul_lift_id = u.lift_id 
 //     AND ul.ul_equip_id = u.equipment_id 
 //     AND ul.liftDate = to_char(u."createdAt", 'MM-DD-YYYY')
-// LEFT JOIN lift_primary_muscle lpm ON lpm.lift_m_id = ul.ul_lift_id
+// LEFT JOIN lift_primary_muscle lpm ON lpm.lift_m_id = ap.wkout_id
 // LEFT JOIN lift_muscle_associations lma ON lpm.lift_m_id = lma.lift_m_id
 // WHERE (ul.ul_user_id = ${req.user.id} OR ul.ul_user_id IS NULL)
 // AND ap.${req.query.column} ILIKE '%${req.query.term}%'
-
